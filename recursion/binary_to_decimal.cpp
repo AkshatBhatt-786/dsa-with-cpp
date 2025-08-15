43a9a0c1dfb1bbf0baee8a92a6381f6d9a013f91
Bhatt Akshat <akshatbhatt0786@gmail.com>
2025-08-15 23:47:24 +0530

    Binary to Decimal Conversion using Recursion

---
+++ recursion/binary_to_decimal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int binary_to_decimal(const string &bin, int index) {
+    if (index == bin.size()) 
+        return 0;
+    return (bin[index] - '0') * pow(2, bin.size() - index - 1) 
+           + binary_to_decimal(bin, index + 1);
+}
+
+int main() {
+    string binary = "0100";
+    cout << "Decimal: " << binary_to_decimal(binary, 0) << endl;
+    return 0;
+}

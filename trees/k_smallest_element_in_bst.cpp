9785a0864c60c6775e14a9d402db96a73bd990c0
Bhatt Akshat <akshatbhatt0786@gmail.com>
2025-08-26 23:14:17 +0530

    Finding smallest 3rd element in bst

---
+++ trees/k_smallest_element_in_bst.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int solve(BinaryTreeNode<int> *root, int &i, int k)
+{
+    // base condition
+    if (root == NULL)
+        return -1;
+    
+    // L
+    int left = solve(root->left, i, k);
+    
+    if(left != -1)
+    {
+        return left;
+    }
+    // N
+    i++;
+
+    if(i == k)
+    {
+        return root->data;
+    }
+
+    // R
+    return solve(root->right, i, k);
+}
+
+int kthSmallest(BinaryTreeNode<int> *root, int &i, int k)
+{
+    int i=0;
+    int ans = solve(root, i, k);
+    return ans;
+}

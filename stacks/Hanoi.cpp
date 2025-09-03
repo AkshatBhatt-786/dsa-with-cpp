b83adb27465fe420b594a68ef023ce71e2b4cbe2
Bhatt Akshat <akshatbhatt0786@gmail.com>
2025-09-03 21:57:33 +0530

    Tower of Hanoi

---
+++ stacks/Hanoi.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// Function to move disks
+void towerOfHanoi(int n, char source, char destination, char helper) {
+    // If there is only 1 disk, move it directly
+    if (n == 1) {
+        cout << "Move disk 1 from " << source << " to " << destination << endl;
+        return;
+    }
+
+    // Step 1: Move (n-1) disks from source to helper
+    towerOfHanoi(n - 1, source, helper, destination);
+
+    // Step 2: Move the biggest disk (nth disk) from source to destination
+    cout << "Move disk " << n << " from " << source << " to " << destination << endl;
+
+    // Step 3: Move (n-1) disks from helper to destination
+    towerOfHanoi(n - 1, helper, destination, source);
+}
+
+int main() {
+    int n;
+    cout << "Enter number of disks: ";
+    cin >> n;
+
+    cout << "\nSteps to solve Tower of Hanoi:\n";
+    // A = starting rod, C = ending rod, B = helper rod
+    towerOfHanoi(n, 'A', 'C', 'B');
+
+    return 0;
+}

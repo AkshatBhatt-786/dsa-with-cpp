e47c37658ffdf79a6b947eaad9f5e4f308562b89
Bhatt Akshat <akshatbhatt0786@gmail.com>
2026-02-23 21:26:12 +0530

    Max Sliding Window Problem using greedy approach

---
+++ InterviewPrep/maxSlidingWindow.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+using namespace std;
+
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    deque<int> dq;  // stores indices
+    vector<int> result;
+
+    for (int i = 0; i < nums.size(); i++) {
+
+        // Remove indices out of window
+        if (!dq.empty() && dq.front() == i - k)
+            dq.pop_front();
+
+        // Maintain decreasing order
+        while (!dq.empty() && nums[dq.back()] < nums[i])
+            dq.pop_back();
+
+        dq.push_back(i);
+
+        // Start storing results after first window
+        if (i >= k - 1)
+            result.push_back(nums[dq.front()]);
+    }
+
+    return result;
+}
+
+int main() {
+    vector<int> nums = {1,3,-1,-3,5,3,6,7};
+    int k = 3;
+
+    vector<int> ans = maxSlidingWindow(nums, k);
+
+    for (int x : ans)
+        cout << x << " ";
+
+    return 0;
+}

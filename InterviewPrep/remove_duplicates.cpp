ec38ca4fdfa6af0ba0fcb4c87c47b25c13018930
Bhatt Akshat <akshatbhatt0786@gmail.com>
2026-02-27 22:51:21 +0530

    Remove duplicates from a string

---
+++ InterviewPrep/remove_duplicates.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> last(26);
+        vector<bool> used(26, false);
+        
+        for (int i = 0; i < s.size(); i++)
+            last[s[i] - 'a'] = i;
+        
+        stack<char> st;
+        
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            
+            if (used[c - 'a']) continue;
+            
+            while (!st.empty() && 
+                   st.top() > c && 
+                   last[st.top() - 'a'] > i) {
+                used[st.top() - 'a'] = false;
+                st.pop();
+            }
+            
+            st.push(c);
+            used[c - 'a'] = true;
+        }
+        
+        string result = "";
+        while (!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+        
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
